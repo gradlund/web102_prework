31debb1c5556d6a65dac84ab73cde7e672936a3a
Grace Radlund <gradlund@my.gcu.edu>
2023-01-09 13:05:53 -0700

    Updated top and runner up games

--- index.js
+++ index.js
@@ -178,7 +178,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [first, second, ...others] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const top = document.createElement("p");
+top.innerHTML = first.name;
+firstGameContainer.append(top);
 
 // do the same for the runner up item
+const runnerUp = document.createElement("p");
+runnerUp.innerHTML = second.name;
+secondGameContainer.append(runnerUp);
